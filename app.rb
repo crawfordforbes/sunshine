5a34d042f3887af14c0346ab03d20a82b4d15294
crawfordforbes <crawford.forbes@gmail.com>
2015-02-24 21:20:23 -0500

    hosting

--- app.rb
+++ app.rb
@@ -11,6 +11,40 @@ require './lib/connection'
 require 'active_record'
 require 'bcrypt'
 include FileUtils::Verbose
+enable :sessions
+
+def authenticated?
+	session[:valid_user] == true
+end
+
+get '/admin/login' do
+	erb :"admin/login"
+end
+
+post '/admin/user' do
+	if params[:password] != params[:pass_confirm]
+		redirect '/admin/login'
+	else
+		pass = BCrypt::Password.create(params[:password])
+		session[:valid_user] = true
+		User.create(name: params[:name], password_digest: pass)
+		redirect '/admin'
+	end
+end
+
+post '/admin/session' do
+	user = User.find_by(name: params[:name])
+	if user
+		if BCrypt::Password.new(user.password_digest) == params[:password]
+			session[:valid_user] = true
+			redirect '/admin'
+		else
+			redirect '/admin/login'
+		end
+	else
+		redirect '/admin/login'
+	end
+end
 
 #landing page for user
 get '/' do
@@ -20,13 +54,18 @@ end
 
 # CRUD pics
 get '/admin/pics' do
+	if authenticated?
 	puts "GET ADMIN/PICS"
 	pics = Pic.all()
 	erb :"admin/pics", locals: {pics: pics}
+else
+	redirect '/admin/login'
+end
 end
 
 # upload a pic, save the file in public, and add the url and carousel status to the pics table
 post '/admin/pic' do
+		if authenticated?
 	puts "POST ADMIN/PIC"
 
 	if params[:carousel_selection]
@@ -38,10 +77,14 @@ post '/admin/pic' do
 	cp(tempfile.path, "./public/#{filename}")
 	Pic.create(url: filename, carousel: car)
 	redirect '/admin/pics'
+	else
+	redirect '/admin/login'
+end
 end
 
 # update carousel status of each pic
 put '/admin/pics' do
+		if authenticated?
 	checked_string = params.keys[0].split(',')
 	checked = []
 	checked_string.each do |id| 
@@ -64,60 +107,96 @@ put '/admin/pics' do
 		end
 	end
 	redirect '/admin/pics'
+	else
+	redirect '/admin/login'
+end
 end
 
 # show an individual pic
 get '/admin/pic/:id' do
+		if authenticated?
 	pic = Pic.find_by(id: params[:id])
 	erb :"admin/pic", locals: {pic: pic}
+	else
+	redirect '/admin/login'
+end
 end
 
 #DESTROY!
 delete '/admin/pic/:id' do
+		if authenticated?
 	pic = Pic.find_by(id: params[:id])
 	filename = pic.url
 	pic.delete
 	File.delete("./public/#{filename}")
 	redirect '/admin/pics'
+	else
+	redirect '/admin/login'
+end
 end
 
 #CRUD posts
 get '/admin/posts' do
+		if authenticated?
 	posts = Post.all()
 	erb :"admin/posts", locals: {posts: posts}
+	else
+	redirect '/admin/login'
+end
 end
 
 #show individual post
 get '/admin/post/:id' do
+		if authenticated?
 	post = Post.find_by(id: params[:id])
 	pics = Pic.all()
 	erb :"admin/post", locals: {post: post, pics: pics}
+	else
+	redirect '/admin/login'
+end
 end
 
 #update post
 put '/admin/post' do
+		if authenticated?
 	puts params
 	post = Post.find_by(id: params[:id].to_i)
 	post.update(title: params[:title], story: params[:story])
 	redirect '/admin/posts'
+	else
+	redirect '/admin/login'
+end
 end
 
 #create post
 post '/admin/post' do
+		if authenticated?
 	Post.create(title: params[:title], story: params[:story])
 	redirect '/admin/posts'
+	else
+	redirect '/admin/login'
+end
 end
 
 #archive deleted post, delete post in regular post table
 delete '/admin/post/:id' do
+		if authenticated?
 	post = Post.find_by(id: params[:id])
 	Oldpost.create(title: post.title, story: post.story)
 	post.delete
 	redirect '/admin/posts'
+	else
+	redirect '/admin/login'
 end
+end
+
 #landing page for admin
 get '/admin' do
+		if authenticated?
 	pics = Pic.all()
 	erb :"admin/admin", locals: {pics: pics}
+	else
+	redirect '/admin/login'
+end
 end
 
