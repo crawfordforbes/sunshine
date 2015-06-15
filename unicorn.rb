16d8101b47df13aa9d0432b3fd02efac265943f6
crawfordforbes <crawford.forbes@gmail.com>
2015-06-15 13:53:35 -0400

    production version

---
+++ unicorn.rb
@@ -0,0 +1,7 @@
+working_directory "/root/sunshine"
+pid "/root/sunshine/pids/unicorn.pid"
+stderr_path "/root/sunshine/logs/unicorn.log"
+stdout_path "/root/sunshine/logs/unicorn.log"
+listen "/tmp/unicorn.myapp.sock"
+worker_processes 2
+timeout 30
